68442d03a8010fc3803f3a68047699a941289dd9
diegoo <rafael2002diego@gmail.com>
2024-08-04 11:12:52 -0700

    Drop and Drop with React dnd

--- src/components/Profile/EditGallery.tsx
+++ src/components/Profile/EditGallery.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import { trpc } from '@/lib/trpc/client';
 import Image from 'next/image';
-import CreatePostForm from './CreatePostForm';
+// import CreatePostForm from './CreatePostForm';
+import { getColumnSpan } from './Gallery';
+import { useDrag, useDrop, DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
-export default function EditGallery({ clerkId }: { clerkId: string }) {
-  const { data: user_images, isLoading: isLoadingImages } =
-    trpc.images.getAllImages.useQuery({ clerkId: clerkId });
+const ItemType = {
+  IMAGE: 'image',
+};
 
+interface DraggableImageProps {
+  image: {
+    url: string;
+    id: string;
+    imageWidth: number | null;
+    imageHeight: number | null;
+  };
+  index: number;
+  moveImage: (fromIndex: number, toIndex: number) => void;
+}
+
+interface DragItem {
+  type: string;
+  id: string;
+  index: number;
+}
+
+const DraggableImage = ({ image, index, moveImage }: DraggableImageProps) => {
+  const ref = React.useRef(null);
   const deleteImage = trpc.images.deleteImage.useMutation();
 
   const handleDeleteImage = async (imageId: string) => {
@@ -15,6 +37,79 @@ export default function EditGallery({ clerkId }: { clerkId: string }) {
     console.log(res);
     console.log('Deleted Image');
     // window.location.reload();
+  };
+
+  const [, drop] = useDrop({
+    accept: ItemType.IMAGE,
+    hover(item: DragItem) {
+      if (!ref.current) {
+        return;
+      }
+      const dragIndex = item.index;
+      const hoverIndex = index;
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+      moveImage(dragIndex, hoverIndex);
+      item.index = hoverIndex;
+    },
+  });
+
+  const [{ isDragging }, drag] = useDrag({
+    type: ItemType.IMAGE,
+    item: { type: ItemType.IMAGE, id: image.id, index },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
+  drag(drop(ref));
+
+  return (
+    <div
+      ref={ref}
+      className={`relative flex ${getColumnSpan(
+        image.imageWidth || 0,
+        image.imageHeight || 0
+      )}`}
+      style={{ opacity: isDragging ? 0.5 : 1 }}
+    >
+      <Image
+        src={image.url}
+        alt=""
+        layout="responsive"
+        width={image.imageHeight || 0}
+        height={image.imageWidth || 0}
+        objectFit="cover"
+        className="rounded-md"
+      />
+      <button
+        className="absolute right-3 top-3 z-30 flex h-6 w-6 items-center justify-center rounded-full border border-gray-400 bg-white text-xs text-black hover:bg-red-400"
+        onClick={() => handleDeleteImage(image.id)}
+      >
+        X
+      </button>
+    </div>
+  );
+};
+
+export default function EditGallery({ clerkId }: { clerkId: string }) {
+  const { data: user_images, isLoading: isLoadingImages } =
+    trpc.images.getAllImages.useQuery({ clerkId: clerkId });
+
+  const [images, setImages] = React.useState(user_images || []);
+
+  React.useEffect(() => {
+    if (user_images) {
+      setImages(user_images);
+    }
+  }, [user_images]);
+
+  const moveImage = (fromIndex: number, toIndex: number) => {
+    const updatedImages = [...images];
+    const [movedImage] = updatedImages.splice(fromIndex, 1);
+    updatedImages.splice(toIndex, 0, movedImage);
+    setImages(updatedImages);
   };
 
   if (isLoadingImages) {
@@ -26,28 +121,19 @@ export default function EditGallery({ clerkId }: { clerkId: string }) {
   }
 
   return (
-    <main className="flex flex-col gap-4">
-      <div className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-4">
-        {user_images.map((image) => (
-          <div key={image.id} className="relative flex h-72 gap-4 p-4">
-            <Image
-              className="rounded-sm border border-black object-cover"
-              src={image.url}
-              alt="Photography Image"
-              fill
+    <DndProvider backend={HTML5Backend}>
+      <main className="flex flex-col gap-4">
+        <div className="grid grid-flow-row-dense grid-cols-12 gap-5">
+          {images.map((image, index) => (
+            <DraggableImage
+              key={image.id}
+              image={image}
+              index={index}
+              moveImage={moveImage}
             />
-            <button
-              className="absolute -right-3 -top-3 z-30 flex h-6 w-6 items-center justify-center rounded-full border border-gray-400 bg-white text-xs  text-black hover:bg-red-400"
-              onClick={() => handleDeleteImage(image.id)}
-            >
-              X
-            </button>
-          </div>
-        ))}
-        <div className="relative flex h-72 gap-4 rounded-sm border border-black">
-          <CreatePostForm />
+          ))}
         </div>
-      </div>
-    </main>
+      </main>
+    </DndProvider>
   );
 }
--- src/components/Profile/Gallery.tsx
+++ src/components/Profile/Gallery.tsx
@@ -2,6 +2,14 @@ import React from 'react';
 import { trpc } from '@/lib/trpc/client';
 import Image from 'next/image';
 
+export const getColumnSpan = (width: number, height: number) => {
+  const aspectRatio = width / height;
+  if (aspectRatio > 1.5) return 'col-span-8'; // Wide images
+  if (aspectRatio > 1) return 'col-span-6'; // Landscape images
+  if (aspectRatio > 0.75) return 'col-span-4'; // Square-ish images
+  return 'col-span-4'; // Portrait images
+};
+
 export default function Gallery({ clerkId }: { clerkId: string }) {
   const { data: user_images, isLoading: isLoadingImages } =
     trpc.images.getAllImages.useQuery({ clerkId: clerkId });
@@ -14,14 +22,6 @@ export default function Gallery({ clerkId }: { clerkId: string }) {
     return <div>No Images Available for this User</div>;
   }
 
-  const getColumnSpan = (width: number, height: number) => {
-    const aspectRatio = width / height;
-    if (aspectRatio > 1.5) return 'col-span-8'; // Wide images
-    if (aspectRatio > 1) return 'col-span-6'; // Landscape images
-    if (aspectRatio > 0.75) return 'col-span-4'; // Square-ish images
-    return 'col-span-4'; // Portrait images
-  };
-
   return (
     <main className="flex flex-col gap-4">
       <h2 className="mb-10 mt-10 font-mono text-xl font-bold">Projects</h2>
@@ -32,7 +32,7 @@ export default function Gallery({ clerkId }: { clerkId: string }) {
             image.imageHeight || 0
           );
           return (
-            <div key={image.id} className={`flex ${columnSpan}`}>
+            <div key={image.id} className={`relative flex ${columnSpan}`}>
               <Image
                 src={image.url}
                 alt=""
